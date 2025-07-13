b62a75f550e2e655ce700629a2374fc74bfd91d1
Todd Chase <toddchase38@gmail.com>
2025-07-13 11:48:53 -0400

    GUI is complete

--- src/guis/BankingAppGui.java
+++ src/guis/BankingAppGui.java
@@ -33,13 +33,54 @@ public class BankingAppGui extends BaseFrame {
 
         // create current balance label
         JLabel currentBalanceLabel = new JLabel("Current Balance");
-        currentBalanceLabel.setBounds(0, 80, getWidth()-10, 30);
-        currentBalanceLabel.setFont(new Font("Dialog", Font.BOLD, 22));
+        currentBalanceLabel.setBounds(0, 80, getWidth() - 10, 30);
+        currentBalanceLabel.setFont(new Font("Dialog", Font.BOLD, 28));
         currentBalanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(currentBalanceLabel);
 
         // create current balance field
-        
+        currentBalanceField = new JTextField("$" + user.getCurrentBalance());
+        currentBalanceField.setBounds(15, 120, getWidth() - 50, 40);
+        currentBalanceField.setFont(new Font("Dialog", Font.BOLD, 22));
+        currentBalanceField.setHorizontalAlignment(SwingConstants.RIGHT);
+        currentBalanceField.setEditable(false);
+        add(currentBalanceField);
+
+        // deposit button
+        JButton depositButton = new JButton("Deposit");
+        depositButton.setBounds(15, 250, getWidth() - 50, 50);
+        depositButton.setFont(new Font("Dialog", Font.BOLD, 28));
+        add(depositButton);
+
+        // withdraw button
+        JButton withdrawButton = new JButton("Withdraw");
+        withdrawButton.setBounds(15, 180, getWidth() - 50, 50);
+        withdrawButton.setFont(new Font("Dialog", Font.BOLD, 28));
+        add(withdrawButton);
+
+        // past transaction button
+        JButton pastTransactionButton = new JButton("Past Transaction");
+        pastTransactionButton.setBounds(15, 320, getWidth() - 50, 50);
+        pastTransactionButton.setFont(new Font("Dialog", Font.BOLD, 28));
+        add(pastTransactionButton);
+
+        // transfer button
+        JButton transferButton = new JButton("Transfer");
+        transferButton.setBounds(15, 390, getWidth() - 50, 50);
+        transferButton.setFont(new Font("Dialog", Font.BOLD, 28));
+        add(transferButton);
+
+        // logout button
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setBounds(15, 500, getWidth() - 50, 50);
+        logoutButton.setFont(new Font("Dialog", Font.BOLD, 28));
+        add(logoutButton);
+
+
+
+
+
+
 
     }
 }
